ebd193d42a17f2875820d76c380aa976108bcb52
Ciaran McNulty <cmcnulty@inviqa.com>
2015-03-27 16:58:04 +0000

    Ciaran is no longer cool

--- web/index.php
+++ web/index.php
@@ -14,7 +14,7 @@ $app->register(new Silex\Provider\MonologServiceProvider(), array(
 
 $app->get('/', function() use($app) {
   $app['monolog']->addDebug('logging output.');
-  return 'Ciaran is cool';
+  return 'Ciaran is no longer';
 });
 
 $app->run();
